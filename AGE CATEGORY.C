0ad92ae234d77bceba70a0c26ef6bde3f9bac9a6
JatinBamal <jatinbamal689@gmail.com>
2026-03-23 23:25:35 +0530

    AGE CATEGORY.C

--- AGE CATEGORY.C
+++ AGE CATEGORY.C
@@ -3,14 +3,10 @@
 int main() {
     int arr[5];
     int i;
-
-    // Accepting elements
     printf("Enter 5 elements:\n");
     for(i = 0; i < 5; i++) {
         scanf("%d", &arr[i]);
     }
-
-    // Displaying elements
     printf("Array elements are:\n");
     for(i = 0; i < 5; i++) {0
         printf("%d ", arr[i]);
